f111fecc38d86528cea5bab75cea07cc622beb3b
AntoniniP <pa.antonini@outlook.com>
2021-03-07 17:08:30 +0000

    Introducing templates for dashboards

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,8 @@ from flask import Flask
 from flask.helpers import get_root_path
 from flask_login import login_required
 
+from flask import Flask, render_template_string
+
 from config import BaseConfig
 
 
@@ -10,32 +12,88 @@ def create_app():
     server = Flask(__name__)
     server.config.from_object(BaseConfig)
 
-    register_dashapps(server)
     register_extensions(server)
     register_blueprints(server)
+    register_dashapps(server)
 
     return server
 
 
+def _get_index_string(template):
+    """
+    Replace the following 'commented-out' placeholders in my 'dash.html' 
+    Flask template with the placeholders that Dash requires, for modifying the 
+    'index_string' of the dash app
+    """
+    template = template.replace(r'<!-- %metas% -->', r'{%metas%}')
+    template = template.replace(r'<!-- %title% -->', r'{%title%}')
+    template = template.replace(r'<!-- %favicon% -->', r'{%favicon%}')
+    template = template.replace(r'<!-- %css% -->', r'{%css%}')
+    template = template.replace(r'<!-- %app_entry% -->', r'{%app_entry%}')
+    template = template.replace(r'<!-- %config% -->', r'{%config%}')
+    template = template.replace(r'<!-- %scripts% -->', r'{%scripts%}')
+    template = template.replace(r'<!-- %renderer% -->', r'{%renderer%}')
+
+    return template
+
+
+# def register_dashapps(app):
+#     from app.dashapp1.layout import layout
+#     from app.dashapp1.callbacks import register_callbacks
+
+#     # Meta tags for viewport responsiveness
+#     meta_viewport = {"name": "viewport", "content": "width=device-width, initial-scale=1, shrink-to-fit=no"}
+
+#     dashapp1 = dash.Dash(__name__,
+#                          server=app,
+#                          url_base_pathname='/dashboard/',
+#                          assets_folder=get_root_path(__name__) + '/dashboard/assets/',
+#                          meta_tags=[meta_viewport])
+
+#     with app.app_context():
+#         dashapp1.title = 'Dashapp 1'
+#         dashapp1.layout = layout
+#         register_callbacks(dashapp1)
+
+#     _protect_dashviews(dashapp1)
+
+
 def register_dashapps(app):
+    """
+    Register Dash apps with the Flask app
+    """
+
     from app.dashapp1.layout import layout
     from app.dashapp1.callbacks import register_callbacks
 
     # Meta tags for viewport responsiveness
-    meta_viewport = {"name": "viewport", "content": "width=device-width, initial-scale=1, shrink-to-fit=no"}
+    meta_viewport = {
+        "name": "viewport", 
+        "content": "width=device-width, initial-scale=1, shrink-to-fit=no"
+    }
 
     dashapp1 = dash.Dash(__name__,
-                         server=app,
-                         url_base_pathname='/dashboard/',
-                         assets_folder=get_root_path(__name__) + '/dashboard/assets/',
-                         meta_tags=[meta_viewport])
+        server=app,
+        url_base_pathname='/dash/',
+        assets_folder=get_root_path(__name__) + '/assets/',
+        meta_tags=[meta_viewport], 
+    )
 
     with app.app_context():
-        dashapp1.title = 'Dashapp 1'
+        # Render my Flask template to get a special 'index_string' for the Dash app
+        path = get_root_path(__name__) + "/templates/dash.html"
+        with open(path, 'r') as f:
+            template_string = render_template_string(f.read())
+        index_string = _get_index_string(template_string)
+        dashapp1.index_string = index_string
+
+        dashapp1.title = 'CHANGING THE LANDSCAPE'
         dashapp1.layout = layout
         register_callbacks(dashapp1)
 
     _protect_dashviews(dashapp1)
+
+    return app
 
 
 def _protect_dashviews(dashapp):
